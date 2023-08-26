67a7fdcced7cf3bbbf56ae73df6d90a52f5e22e9
Kaiwa-Jun <kj.11235813213455@gmail.com>
2023-08-27 04:25:21 +0900

    :sparkles: #1 各問題のフェイクポケモン等の数の調整

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -7,46 +7,73 @@ def new
     redirect_to quiz_result_path and return
   end
 
-  difficulty_level = [session[:question_number], 3].min
-
   excluded_types = ['unknown', 'shadow']
   @type = Type.where.not(name: excluded_types).order("RAND()").first
 
-  # real_pokemons の選択条件を調整（fake_type_idも考慮）
-  real_pokemons = Pokemon.where('(type_id = ? OR secondary_type_id = ?)', @type.id, @type.id)
-                          .where('fake_type_id IS NULL OR fake_type_id != ?', @type.id)
-                          .order('RAND()').limit(6).to_a
-
-  # fake_pokemons の選択に fake_type_id を考慮
-  fake_pokemons = Pokemon.where(fake_type_id: @type.id, is_fake: true).order('RAND()').limit(3).to_a
-
-  if fake_pokemons.count < 3
-    extra_real_pokemons = Pokemon.where(type_id: @type.id)
-                                 .where('is_fake = ? OR is_fake IS NULL', false)
-                                 .where.not(id: real_pokemons.map(&:id))
-                                 .order('RAND()').limit(3 - fake_pokemons.count).to_a
-    real_pokemons += extra_real_pokemons
+  # @typeがnilでないことを確認
+  if @type.nil?
+    redirect_to some_error_page_path and return
   end
 
-  @pokemons = (real_pokemons + fake_pokemons).shuffle
+  # 質問数に基づいてフェイクポケモン、同タイプポケモン、異なるタイプのポケモンの数を決定
+  fake_pokemon_count, same_type_pokemon_count, different_type_pokemon_count = case session[:question_number]
+  when 1
+    [0, 9, 0]
+  when 2
+    [0, 6, 3]
+  when 3
+    [1, 6, 2]
+  when 4
+    [2, 5, 2]
+  when 5
+    [3, 6, 0]
+  else
+    [0, 0, 0]  # Default case, you can handle this differently if needed
+  end
+
+  # fake_pokemons の選択に fake_type_id と image_url を考慮
+  fake_pokemons = Pokemon.where(fake_type_id: @type.id, is_fake: true)
+                          .where('image_url IS NOT NULL')
+                          .order('RAND()').limit(fake_pokemon_count).to_a
+
+  # 同タイプのreal_pokemons の数を調整
+  real_pokemons_same_type = Pokemon.where('(type_id = ? OR secondary_type_id = ?) AND image_url IS NOT NULL', @type.id, @type.id)
+                                   .where('fake_type_id IS NULL OR fake_type_id != ?', @type.id)
+                                   .order('RAND()').limit(same_type_pokemon_count).to_a
+
+  # 異なるタイプのreal_pokemons の数を調整
+  real_pokemons_diff_type = Pokemon.where.not('(type_id = ? OR secondary_type_id = ?)', @type.id, @type.id)
+                                   .where('fake_type_id IS NULL OR fake_type_id != ?', @type.id)
+                                   .where('image_url IS NOT NULL')
+                                   .order('RAND()').limit(different_type_pokemon_count).to_a
+
+  @pokemons = (real_pokemons_same_type + real_pokemons_diff_type + fake_pokemons).shuffle
+
+  # デバッグ出力
+  puts "Debugging in new action:"
+  puts "Selected Type: #{@type.name}"
+  @pokemons.each do |pokemon|
+    puts "Pokemon Name: #{pokemon.name}, Type ID: #{pokemon.type_id}, Secondary Type ID: #{pokemon.secondary_type_id}, Fake Type ID: #{pokemon.fake_type_id}, Image URL: #{pokemon.image_url}"
+  end
+
   session[:pokemons] = @pokemons.map { |pokemon| pokemon.id }
-  session[:correct_pokemons] = real_pokemons.map { |pokemon| pokemon.id }
+  session[:correct_pokemons] = real_pokemons_same_type.map { |pokemon| pokemon.id }
 
   @current_question_number = session[:question_number]
   @total_questions = 5
   session[:type_id] = @type.id
   session[:type_name] = @type.name
 
-  # デバッグ出力
+  # その他のデバッグ出力
   puts "Debugging in new action:"
   puts "session[:pokemons]: #{session[:pokemons].inspect}"
   puts "session[:correct_pokemons]: #{session[:correct_pokemons].inspect}"
   puts "session[:type_id]: #{session[:type_id]}"
-
   # fraxureのID（例えば、fraxureがID 1000だとする）
   fraxure_id = 1000 # 実際のIDに変更してください
   puts "Is fraxure in correct_pokemons?: #{session[:correct_pokemons].include?(fraxure_id)}"
 end
+
 
 
 def confirm
