f7a937ecc07fedba59b5a24f475c69033113f0aa
Kaiwa-Jun <kj.11235813213455@gmail.com>
2023-08-27 03:29:33 +0900

    :sparkles: #1 fake_type_idの設定修正

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,34 +1,53 @@
 class QuizController < ApplicationController
   before_action :set_question_number
 
-  def new
-    @total_questions = 5
-    if session[:question_number] > @total_questions
-      redirect_to quiz_result_path and return
-    end
-
-    difficulty_level = [session[:question_number], 3].min
-
-    excluded_types = ['unknown', 'shadow']
-    @type = Type.where.not(name: excluded_types).order("RAND()").first
-
-    real_pokemons = Pokemon.where(type_id: @type.id).where('is_fake = ? OR is_fake IS NULL', false).order('RAND()').limit(6).to_a
-    fake_pokemons = Pokemon.where(fake_type_id: @type.id, is_fake: true).order('RAND()').limit(3).to_a
-
-    if fake_pokemons.count < 3
-      extra_real_pokemons = Pokemon.where(type_id: @type.id).where('is_fake = ? OR is_fake IS NULL', false).where.not(id: real_pokemons.map(&:id)).order('RAND()').limit(3 - fake_pokemons.count).to_a
-      real_pokemons += extra_real_pokemons
-    end
-
-    @pokemons = (real_pokemons + fake_pokemons).shuffle
-    session[:pokemons] = @pokemons.map { |pokemon| pokemon.id }
-    session[:correct_pokemons] = real_pokemons.map { |pokemon| pokemon.id }
-
-    @current_question_number = session[:question_number]
-    @total_questions = 5
-    session[:type_id] = @type.id
-    session[:type_name] = @type.name
+def new
+  @total_questions = 5
+  if session[:question_number] > @total_questions
+    redirect_to quiz_result_path and return
   end
+
+  difficulty_level = [session[:question_number], 3].min
+
+  excluded_types = ['unknown', 'shadow']
+  @type = Type.where.not(name: excluded_types).order("RAND()").first
+
+  # real_pokemons の選択条件を調整（fake_type_idも考慮）
+  real_pokemons = Pokemon.where('(type_id = ? OR secondary_type_id = ?)', @type.id, @type.id)
+                          .where('fake_type_id IS NULL OR fake_type_id != ?', @type.id)
+                          .order('RAND()').limit(6).to_a
+
+  # fake_pokemons の選択に fake_type_id を考慮
+  fake_pokemons = Pokemon.where(fake_type_id: @type.id, is_fake: true).order('RAND()').limit(3).to_a
+
+  if fake_pokemons.count < 3
+    extra_real_pokemons = Pokemon.where(type_id: @type.id)
+                                 .where('is_fake = ? OR is_fake IS NULL', false)
+                                 .where.not(id: real_pokemons.map(&:id))
+                                 .order('RAND()').limit(3 - fake_pokemons.count).to_a
+    real_pokemons += extra_real_pokemons
+  end
+
+  @pokemons = (real_pokemons + fake_pokemons).shuffle
+  session[:pokemons] = @pokemons.map { |pokemon| pokemon.id }
+  session[:correct_pokemons] = real_pokemons.map { |pokemon| pokemon.id }
+
+  @current_question_number = session[:question_number]
+  @total_questions = 5
+  session[:type_id] = @type.id
+  session[:type_name] = @type.name
+
+  # デバッグ出力
+  puts "Debugging in new action:"
+  puts "session[:pokemons]: #{session[:pokemons].inspect}"
+  puts "session[:correct_pokemons]: #{session[:correct_pokemons].inspect}"
+  puts "session[:type_id]: #{session[:type_id]}"
+
+  # fraxureのID（例えば、fraxureがID 1000だとする）
+  fraxure_id = 1000 # 実際のIDに変更してください
+  puts "Is fraxure in correct_pokemons?: #{session[:correct_pokemons].include?(fraxure_id)}"
+end
+
 
 def confirm
   @total_questions = 5
@@ -36,11 +55,14 @@ def confirm
   selected_pokemons_ids = selected_pokemons_indexes.map { |index| session[:pokemons][index.to_i] }
   correct_type_id = session[:type_id]  # 問題で指定されたタイプID
 
-  # 選択されたポケモンのtype_idを取得
-  selected_pokemons_type_ids = Pokemon.where(id: selected_pokemons_ids).pluck(:type_id)
+    # 選択されたポケモンのtype_id, secondary_type_id, そして fake_type_id を取得
+    selected_pokemons_types = Pokemon.where(id: selected_pokemons_ids).pluck(:type_id, :secondary_type_id, :fake_type_id)
 
-  # 選択された全てのポケモンのtype_idが問題で指定されたタイプIDと一致するか確認
-  is_correct = selected_pokemons_type_ids.all? { |type_id| type_id == correct_type_id }
+# 選択された全てのポケモンが、問題で指定されたタイプIDと一致するか確認
+is_correct = selected_pokemons_types.all? do |type_id, secondary_type_id, fake_type_id|
+  type_id == correct_type_id || secondary_type_id == correct_type_id || fake_type_id == correct_type_id
+end
+
 
   session[:results] << { question: session[:type_name], correct: is_correct, correct_pokemons: session[:correct_pokemons], all_pokemons: session[:pokemons] }
 
@@ -49,12 +71,19 @@ def confirm
   else
     redirect_to quiz_new_path
   end
+
+  # デバッグ出力
+  puts "Debugging in confirm action:"
+  puts "session[:pokemons]: #{session[:pokemons].inspect}"
+  puts "session[:correct_pokemons]: #{session[:correct_pokemons].inspect}"
+  puts "session[:type_id]: #{session[:type_id]}"
 end
+
 
 
   def start
     session[:question_number] = 0
-    session[:results] = [] 
+    session[:results] = []
     redirect_to action: :new
   end
 
