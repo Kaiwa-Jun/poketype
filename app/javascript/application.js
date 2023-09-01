dfa77783251268fa67cd16268988b768f94088f3
Kaiwa-Jun <kj.11235813213455@gmail.com>
2023-09-01 18:01:54 +0900

    database.yml hostの追記

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,11 @@
+/* global Turbo */
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import * as bootstrap from "bootstrap"
+import "@hotwired/turbo-rails";
+import "./controllers";
+import * as bootstrap from "bootstrap";
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (window.Turbo) {
+    Turbo.session.drive = false;
+  }
+});
